436c9f796a43ee603b7494ab0119fe3b0161f533
Jeff-Cortese <jcortese86@gmail.com>
2017-02-13 16:50:08 -0600

    stormpath experimentation

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # helga
+
+Install [node](https://nodejs.org)
+
+Install [serverless](https://serverless.com)
+
+Install dependencies
+````shell
+$ cd server/api
+$ npm install
+````
+
+Compile typescript
+````shell
+$ npm run tsc
+````
+
+Deploy
+````shell
+$ serverless deploy
+````
--- server/api/src/app.ts
+++ server/api/src/app.ts
@@ -4,14 +4,61 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import * as serverlessMiddleware from 'aws-serverless-express/middleware';
+import * as stormpathExpress from 'express-stormpath';
+import * as stormpath from 'stormpath';
 
-const app = express();
+const app: any = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(serverlessMiddleware.eventContext());
 
+let now: any = new Date();
+
+
+/* --- Using the stormpath express*/
+app.use(stormpathExpress.init(app, {
+  apiKey: {
+    id: 'YOUR_KEY_ID',
+    secret: 'YOUR_KEY_SECRET'
+  },
+  application: {
+    href: `YOUR_APPLICATION_URL`
+  }
+}));
+
+app.on('stormpath.ready', function () {
+  let then: any = new Date();
+  console.log(`Stormpath Ready in ${then - now} ms`);
+});
+
+
+/* --- Manually using the stormpath client to authenticate --- */
+app.get('/alternateLogin', (request, response) => {
+  let spClient = new stormpath.Client({
+    apiKey: {
+      id: 'YOUR_KEY_ID',
+      secret: 'YOUR_KEY_SECRET'
+    },
+  });
+
+  spClient.getApplication(`YOUR_APPLICATION_URL`, (err, spApplication) => {
+    let creds = {
+      username: 'fezoha@fun64.com',
+      password: 'Password1'
+    };
+    spApplication.authenticateAccount(creds, (err, result) => {
+      result.getAccount(function (err, account) {
+        console.log(account);
+        response.json(account);
+      });
+    })
+  });
+});
+
+
+/* --- Other example express endpoints --- */
 app.get('/yo', (req, res) => res.json({ name: 'hello jeff' }));
 app.get('/yo/:myParam', (req, res) => res.json({ parameter: req.params.myParam }));
 app.get('/myasync', (req, res) => new Promise((resolve) => resolve(res.json('yay async!'))));
